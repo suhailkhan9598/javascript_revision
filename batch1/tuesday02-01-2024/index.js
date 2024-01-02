46a757f0dc1fe41c7ba6c954e341058c5b002ac5
suhail khan <khansuhail9598@gmail.com>
2024-01-02 17:49:23 +0530

    basic logical and mathematical operator

---
+++ batch1/tuesday02-01-2024/index.js
@@ -0,0 +1,20 @@
+
+
+// function check (number) {
+//     if (number % 2 != 0) {
+//         console.log("odd")
+//     } else {
+//         console.log("even")
+//     }
+// }
+// check(10)
+
+
+let a = 4;
+let b = 2;
+
+
+// let c = (d + a + b);
+console.log(b)
+245
+// console.log(c)
