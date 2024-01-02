ac549757f7ba100c7d98b598c54a5a7c3a321247
suhail khan <khansuhail9598@gmail.com>
2024-01-02 09:57:26 +0530

    update flex property

---
+++ batch1/monday01-01-2024/index.js
@@ -0,0 +1,50 @@
+
+
+// function checkscope () {
+//     let a = 20;
+//     var b = 30;
+//     const c = 40;
+
+//     if (a == 10) {
+//         let a = 50;
+//         a = 100;
+//         var b = 60;
+//         const c = 70
+//         console.log("from block scope" + " " + a, b, c)
+//     }
+//     var b = 100
+//     console.log(a, b, c, "from functional scope")
+
+// }
+// checkscope()
+
+// let x = "hello world" ///srting
+// let y = 342
+// let z = true
+// let a = null
+// let b;
+// let obj1={
+//     name:"manjeet"
+// }
+// let obj2 = {
+//     name: "manjeet"
+// }
+// console.log(typeof obj1, typeof obj2)
+// console.log(obj1,obj2)
+// console.log(typeof x)
+// console.log(typeof y)
+// console.log(typeof z)
+// console.log(typeof a)
+// console.log(typeof b)
+// Create a script that classifies the temperature into "Hot," "Moderate," or "Cold" based on the following criteria: Hot(above 30°C), Moderate(between 15°C and 30°C), Cold(below 15°C).
+
+
+
+let number = 10;
+if (number % 2==0) {
+console.log("even")
+} else {
+    console.log("odd")
+}
+"masai" == "masai"
+"masai"=="Masai"
