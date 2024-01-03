d0831773f73a048548491ce64fd01dd02bf12580
suhail khan <khansuhail9598@gmail.com>
2024-01-03 15:37:11 +0530

    update code for flex

---
+++ batch1/wednesdya03-0302024/operator.js
@@ -0,0 +1,24 @@
+// let a = 10;
+// let b = 15;
+// let c = 2;
+// let d = 5;
+
+// if (a > b && a > c && a > d) {
+//     console.log("a is greater")
+// } else if (b > a && b > c && b > d) {
+//     console.log("b is greater")
+
+// } else if (c > b && c > a && c > d) {
+//     console.log("c is greater")
+
+// } else if (d > b && d > c && d > a) {
+//     console.log("d is greater")
+
+// } else {
+//     confirm.log("not valid")
+// }
+
+let a = 2
+let b = "4"
+let c = (a + +b)
+console.log(c)
