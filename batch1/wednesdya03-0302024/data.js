35dde0fef092cd9c39238ad672be7985ba24f13b
suhail khan <khansuhail9598@gmail.com>
2024-01-05 16:34:10 +0530

    updte react app

--- batch1/wednesdya03-0302024/data.js
+++ batch1/wednesdya03-0302024/data.js
@@ -1,28 +1,31 @@
-let a = 10;
-let b = 150;
-let c = 20;
-let d = 500;
+// let a = 10;
+// let b = 150;
+// let c = 20;
+// let d = 500;
 
-if (a > b) {
-    if (a > c) {
-        if (a > d) {
-            console.log("a is greater");
-        }
-    }
-} else if (b > c) {
-    if (b > d) {
-        if (b > a) {
-            console.log("b is greater");
-        }
-    }
-} else if (c > d) {
-    if (c > b) {
-        if (c > a) {
-            console.log("c is greater");
-        }
-    }
+// if (a > b) {
+//     if (a > c) {
+//         if (a > d) {
+//             console.log("a is greater");
+//         }
+//     }
+// } else if (b > c) {
+//     if (b > d) {
+//         if (b > a) {
+//             console.log("b is greater");
+//         }
+//     }
+// } else if (c > d) {
+//     if (c > b) {
+//         if (c > a) {
+//             console.log("c is greater");
+//         }
+//     }
 
 
-} else {
-    console.log("d is greater");
-}
+// } else {
+//     console.log("d is greater");
+// }
+
+let a = "s"
+a=="suhail"?console.log("name is true"):a=="manish"?console.log("name is false"):console.log("not a valid name")
--- batch1/wednesdya03-0302024/op.js
+++ batch1/wednesdya03-0302024/op.js
@@ -1,10 +1,21 @@
-let a = 10;
-let b = 5;
-let c = 20;
-if (a < b || a> c) {
-    console.log("posible-5")
-} else if (b > a || b < c) {
-    console.log("posible-7")
-} else {
-    console.log("not possible")
+// let a = 10;
+// let b = 5;
+// let c = 20;
+// if (a < b || a> c) {
+//     console.log("posible-5")
+// } else if (b > a || b < c) {
+//     console.log("posible-7")
+// } else {
+//     console.log("not possible")
+// }
+
+const getData = () => {
+    try {
+        let res = axios.get("https://fakestoreapi.com/products")
+        console.log(res)
+        // setData(res.data)
+    } catch (error) {
+console.log(error)
+    }
 }
+getData()
