3d29130effeb13a41846da0d461641ed5e451d39
suhail khan <khansuhail9598@gmail.com>
2024-01-07 12:12:36 +0530

    update

---
+++ batch1/friday05-01-2024/index.js
@@ -0,0 +1,38 @@
+// var i = 1;
+// var sum = 0;
+// while (i <= 10) {
+//     sum = sum + i;
+//     i++;
+// }
+// console.log(sum);
+
+
+// let i=10;
+// while (i!=0) {
+//     console.log("hello from from suhail",i)
+//     i--
+// }
+
+// var guest = 0;
+// while (guest <= 10) {
+//     console.log("Guest", guest);
+//     if (guest == 3) {
+//         break;
+//     }
+//     guest++
+// }
+
+var count = 0;
+while (true) {
+    console.log("Hello");
+    count++;//1//2
+    console.log(count,"29")
+    ++count;//2==3
+    console.log(count,"31")
+
+    if (count > 5) {//false
+        break;
+    }
+    count--;//1 and again//0==2
+    console.log(count,"37")
+}
