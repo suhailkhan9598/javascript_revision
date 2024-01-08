602e32922a4f04fc27ac9e36b42ab293dfd3326a
suhail khan <khansuhail9598@gmail.com>
2024-01-08 21:14:14 +0530

    factorial and for loop

--- batch1/monday-08-01-2024/index.js
+++ batch1/monday-08-01-2024/index.js
@@ -1,8 +1,47 @@
-let i = 0;
-let num = 3;
-let sum=0
-while (i <= num) {
-    sum = sum + i
-    i++
+// let i = 0;
+// let num = 3;
+// let sum=0
+// while (i <= num) {
+//     sum = sum + i
+//     i++
+// }
+// console.log(sum);
+
+
+// var guest = 0;
+// while (guest <= 10) {
+//     console.log("Guest", guest);
+//     if (guest == 3) {
+//         continue;
+//         guest++
+//     }
+//     guest++;
+// }
+
+// var guest = 1;
+// while (guest <= 10) {
+//     if (guest == 3) {
+//         guest++;
+//         continue;
+//     }
+//     console.log("Guest", guest);
+//     guest++;
+// }
+
+// var bag = "";
+// for (var i = 1; i <= 5; i++) {
+//     bag = bag + i + " ";
+// }
+// console.log(bag);
+
+// var bag = "";
+// for (var i = 1; i <= 10; i=i+2) {
+//     bag = bag + i + " ";
+// }
+// console.log(bag);
+
+var fact = 1;
+for (var i = 1; i <= 5; i++) {
+    fact = fact * i;
 }
-console.log(sum);
+console.log(fact);
