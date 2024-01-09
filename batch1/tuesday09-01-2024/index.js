293f4a966441207ec573e19d1d787457f04e482b
suhail khan <khansuhail9598@gmail.com>
2024-01-09 15:47:50 +0530

    nested loop

--- batch1/tuesday09-01-2024/index.js
+++ batch1/tuesday09-01-2024/index.js
@@ -1,13 +1,38 @@
-let sum = 0
-for (let i = 0; i <= 10; i++) {
-    if (i % 2 == 0) {//number check
-        if (i == 6) {
-            // console.log(i,"from 5")
-            continue
-        }
+// let sum = 0
+// for (let i = 0; i <= 10; i++) {
+//     if (i % 2 == 0) {//number check
+//         if (i == 6) {
+//             // console.log(i,"from 5")
+//             continue
+//         }
 
-        sum = sum + i
-        console.log(i)
+//         sum = sum + i
+//         console.log(i)
+//     }
+// }
+// console.log(`sum is ${sum}`);
+
+// let bag = "";
+// let bag1 = "";
+// let arr = "hellosir";
+
+// for (let i = 0; i <= arr.length - 1; i++) {
+//     if (arr[i] === "a"||arr[i]=="i"||arr[i]=="e"||arr[i]=="o") {
+//         bag = bag +arr[i]+" "
+
+//     }
+//     else {
+//         bag1=bag1+arr[i]+" "
+//     }
+// }
+// console.log(bag);
+// console.log(bag1);
+
+
+for (var i = 0; i < 2; i++) {
+    for (var j = 0; j < 3; j++) {
+        console.log("Hello");
     }
+
+console.log("suhail")
 }
-console.log(`sum is ${sum}`);
