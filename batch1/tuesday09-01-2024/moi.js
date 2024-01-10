966cda46b8f5cf28a605d94b8ad50ea200e47cb0
suhail khan <khansuhail9598@gmail.com>
2024-01-10 21:18:47 +0530

    update pattern printing

--- batch1/tuesday09-01-2024/moi.js
+++ batch1/tuesday09-01-2024/moi.js
@@ -3,13 +3,42 @@
 // ***
 // ****
 // *****
-num=5
-for (let row = 0; row <= num; row++){
+// num=5
 
+// for (let row = 0; row <= 5; row++) {
+
+//     let bag = "";
+//     for (let col = 1; col <= row; col++) {
+//         bag = bag + "*" + " "
+//     }
+//     console.log(bag)
+
+// }
+// ****************//
+
+// for (let i = 0; i <= 5; i++) {
+
+//     let bag = "";
+//     for (let j = 1; j <= 5 - i; j++) {
+//         bag=bag+"*"+" "
+//     }
+//     console.log(bag)
+
+// }
+let num = 19;
+for (let i = 1; i <= num; i++) {
     let bag = "";
-    for (let col = 1; col <= num-row; col++){
-        bag=bag+"*"+" "
-    }
-    console.log(bag)
+    for (let j = 1; j <= num; j++) {
+        if (j == Math.ceil(num / 2)) {
+            bag = bag + "#";
 
+        } else if (i == 1) {
+            bag = bag + "*"
+        }
+        else {
+            bag = bag + " "
+        }
+    }
+
+    console.log(bag);
 }
