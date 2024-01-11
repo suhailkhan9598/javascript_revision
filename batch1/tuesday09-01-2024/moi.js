09e8f3441b8c9e75a2cc14e6e3b0e10d404946a5
suhail khan <khansuhail9598@gmail.com>
2024-01-11 18:38:48 +0530

    revision

--- batch1/tuesday09-01-2024/moi.js
+++ batch1/tuesday09-01-2024/moi.js
@@ -11,20 +11,21 @@ for (let row = 0; row <= 5; row++) {
     for (let col = 1; col <= row; col++) {
         bag = bag + "*" + " "
     }
+    
     console.log(bag)
 
 }
 
 
-for (let i = 0; i <= 5; i++) {
+// for (let i = 0; i <= 5; i++) {
 
-    let bag = "";
-    for (let j = 1; j <= 5 - i; j++) {
-        bag=bag+"*"+" "
-    }
-    console.log(bag)
+//     let bag = "";
+//     for (let j = 1; j <= 5 - i; j++) {
+//         bag=bag+"*"+" "
+//     }
+//     console.log(bag)
 
-}
+// }
 // let num = 19;
 // for (let i = 1; i <= num; i++) {
 //     let bag = "";
