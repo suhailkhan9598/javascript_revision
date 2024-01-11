79ea9628406bf6b8f7573a24dfb62de73738d9fa
suhail khan <khansuhail9598@gmail.com>
2024-01-11 22:53:35 +0530

    map reduce

---
+++ batch1/thursday11-0102024/index.js
@@ -0,0 +1,32 @@
+
+// let arr = [1, 2, 3, 4]
+// let newarr = arr.forEach(function(value){
+//     console.log(value)
+//     return value+1
+// })
+// console.log(newarr,"line-6")
+// Example array of numbers
+// let a=numbers.forEach(function (value,index) {
+//     console.log(value*2 +"-"+index);
+
+// });
+
+// numbers.forEach(number => console.log(number));
+
+// let a = numbers.map(function(ele,index){
+//     console.log(ele,index)
+//     return ele+1
+// })
+// console.log(a)
+
+
+// let a = numbers.filter(function (value1,value2) {
+//     console.log(value1,value2)
+//     return value1>2
+// })
+
+const numbers = [1, 2, 3,2];
+let newdata = numbers.reduce(function (value1, value2) {
+    return value1 + value2*value1
+})
+console.log(newdata);
